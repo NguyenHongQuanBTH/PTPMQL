4815c0151bbc80622f344c2611b7d2884bc4d400
NguyenHongQuanBTH <quanto190301@gmail.com>
2023-09-07 17:37:11 +0700

    first MVC

--- DemoMVC/Controllers/HomeController.cs
+++ DemoMVC/Controllers/HomeController.cs
@@ -1,8 +1,8 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using DemoMVC.Models;
+using DemoMvc.Models;
 
-namespace DemoMVC.Controllers;
+namespace DemoMvc.Controllers;
 
 public class HomeController : Controller
 {
--- firstWebMVC/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace firstWebMVC.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
