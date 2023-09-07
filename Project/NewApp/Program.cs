db37b6af48ecd67b89fb01fba1a67a56db9f32e2
NguyenHongQuanBTH <quanto190301@gmail.com>
2023-09-07 17:18:06 +0700

    Thuc hanh C#

--- Project/NewApp/Program.cs
+++ Project/NewApp/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Chao moi nguoi minh la Quan!");
+internal class program
+{
+    private static void Main(string[] args)
+    {
+        int songuyen;
+        float sothuc;
+        string chuoi;
+        char kytu;
+        bool dungsai;
+        songuyen = 95;
+        sothuc = 9.5f;
+        chuoi = "chuoi";
+        kytu = 'Q';
+        dungsai = false;
+        Console.WriteLine("Kieu so nguyen: {0}", songuyen);
+        Console.WriteLine("Kieu so thuc: {0}", sothuc);
+        Console.WriteLine("Kieu chuoi: {0}", chuoi);
+        Console.WriteLine("Kieu ky tu: {0}", kytu);
+        Console.WriteLine("Kieu bool: {0}", dungsai);
+
+    }
+}
