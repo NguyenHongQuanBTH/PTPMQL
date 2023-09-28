ba8e1c2d9d4b7d162683d899b5d0088e928c79a7
NguyenHongQuanBTH <quanto190301@gmail.com>
2023-09-29 04:12:08 +0700

    Continue

--- Project/NewApp/Program.cs
+++ Project/NewApp/Program.cs
@@ -6,7 +6,7 @@ public class Program
     {
         for (int i = 0; i < 12; i ++)
         {
-            if(i == 8) break;
+            if(i == 8) continue;
             System.Console.WriteLine("Vong lap thu {0}", i);
         }
     }
