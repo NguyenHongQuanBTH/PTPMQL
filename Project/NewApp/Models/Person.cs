e696ea31f4715b0cc3307288b724d82b0545d751
NguyenHongQuanBTH <quanto190301@gmail.com>
2023-09-29 02:20:18 +0700

    Console.ReadLine

---
+++ Project/NewApp/Models/Person.cs
@@ -0,0 +1,31 @@
+
+
+namespace NewApp.Models
+{
+    public class Person
+    {
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+    
+    public void EnterData()
+    {
+        System.Console.Write("FullName = ");
+        FullName = Console.ReadLine();
+        System.Console.Write("Address = ");
+        Address = Console.ReadLine();
+        System.Console.Write("Age = ");
+         
+        try{
+            Age = Convert.ToInt16(Console.ReadLine());
+        }catch(Exception e)
+        {
+            Age = 0;
+        }
+    }
+    public void Display()
+        {
+            System.Console.WriteLine("{0} - {1} tuoi", FullName, Address, Age);
+        }
+    }
+}
