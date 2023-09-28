e693147ececb737fea8b3e4f9fae8d9db60d91c5
NguyenHongQuanBTH <quanto190301@gmail.com>
2023-09-29 03:56:30 +0700

    Vong lap for

--- Project/NewApp/Program.cs
+++ Project/NewApp/Program.cs
@@ -1,22 +1,12 @@
 ﻿using System.Collections.Specialized;
 
-internal class Program
+public class Program
 {
     private static void Main (String [] args)
     {
-        int day = 20;
-        switch(day)
+        for (int i = 0; i < 20; i ++)
         {
-            case 1 : System.Console.WriteLine("Chu Nhat"); break;
-            case 2 : System.Console.WriteLine("Thu Hai"); break;
-            case 3 : System.Console.WriteLine("Thu Ba"); break;
-            case 4 : System.Console.WriteLine("Thu Tu"); break;
-            case 5 : System.Console.WriteLine("Thu Nam"); break;
-            case 6 : System.Console.WriteLine("Thu Sau"); break;
-            case 7 : System.Console.WriteLine("Thu Bay"); break;
-            default : 
-            System.Console.WriteLine("Khong phai ngay trong tuan");
-            break;
+            System.Console.WriteLine("Vong lap thu {0}", i);
         }
     }
 }
