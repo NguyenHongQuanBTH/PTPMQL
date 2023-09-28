0d07c0d8464c195ec3f0a7500b187c39b8239e51
NguyenHongQuanBTH <quanto190301@gmail.com>
2023-09-29 03:49:27 +0700

    switch case 1

--- Project/NewApp/Program.cs
+++ Project/NewApp/Program.cs
@@ -4,18 +4,20 @@ internal class Program
 {
     private static void Main (String [] args)
     {
-        int a = 7;
-        if (a < 0)
+        int day = 5;
+        switch(day)
         {
-            System.Console.WriteLine("{0} la so nguyen am", a);
-        }else{
-            if (a % 2 == 0){
-                System.Console.WriteLine("{0} la so nguyen duong chan", a);
-            }else{
-                System.Console.WriteLine("(0) la so nguyen duong le", a);
-            }
+            case 1 : System.Console.WriteLine("Chu Nhat"); break;
+            case 2 : System.Console.WriteLine("Thu Hai"); break;
+            case 3 : System.Console.WriteLine("Thu Ba"); break;
+            case 4 : System.Console.WriteLine("Thu Tu"); break;
+            case 5 : System.Console.WriteLine("Thu Nam"); break;
+            case 6 : System.Console.WriteLine("Thu Sau"); break;
+            case 7 : System.Console.WriteLine("Thu Bay"); break;
+            default : 
+            System.Console.WriteLine("Khong phai ngay trong tuan");
+            break;
         }
-
     }
 }
 
