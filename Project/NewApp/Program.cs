7d3077ff103ab4a04030c5e6b22afa0f645507c6
NguyenHongQuanBTH <quanto190301@gmail.com>
2023-09-29 04:04:08 +0700

    Vong lap do while

--- Project/NewApp/Program.cs
+++ Project/NewApp/Program.cs
@@ -5,11 +5,11 @@ public class Program
     private static void Main (String [] args)
     {
         int a = 1;
-        while(a < 19)
+        do
         {
             System.Console.WriteLine("Vong lap thu {0}", a);
             a++;
-        }
+        }while(a<9);
     }
 }
 
