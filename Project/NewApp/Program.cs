0f6c8b8d1f4fa75abecb51311814c2670c736043
NguyenHongQuanBTH <quanto190301@gmail.com>
2023-10-03 22:36:22 +0700

    Sinh Vien

--- Project/NewApp/Program.cs
+++ Project/NewApp/Program.cs
@@ -4,15 +4,15 @@ public class Program
 {
     private static void Main(string[] args)
     {
-        Person ps1 = new Person();
-        Person ps2 = new Person();
-        ps1.FullName = "Nguyen Hong Quan";
-        ps1.Address = "Quang Ninh";
-        ps1.Age = 23;
-        ps1.Display();
-        ps2.FullName = "Tran Hong Hanh";
-        ps2.Address = "Thai Binh";
-        ps2.Age = 22;
-        ps2.Display();
+        SinhVien sv1 = new SinhVien();
+        SinhVien sv2 = new SinhVien();
+        sv1.FullName = "Nguyen Hong Quan";
+        sv1.Address = "Quang Ninh";
+        sv1.Age = 23;
+        sv1.Display();
+        sv2.FullName = "Tran Hong Hanh";
+        sv2.Address = "Thai Binh";
+        sv2.Age = 22;
+        sv2.Display();
     }
 }
