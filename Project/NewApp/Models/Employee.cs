94f356b285356563cf59f6b0051dd4f077f1f05d
NguyenHongQuanBTH <quanto190301@gmail.com>
2023-10-04 22:49:28 +0700

    Thuc hanh Employee2

--- Project/NewApp/Models/Employee.cs
+++ Project/NewApp/Models/Employee.cs
@@ -4,24 +4,29 @@ namespace NewApp.Models
     {
         public string MaNhanVien { get; set; }
         public string TenNhanVien { get; set; }
+        public string Address { get; set; }
         public int Age { get; set; }
-        public float HeSoLuong { get; set; }
+        
 
     
-    public void EnterData()
+    public Employee()
     {
-        System.Console.Write("MaNhanVien = ");
-        MaNhanVien = Console.ReadLine();
-        System.Console.Write("TenNhanVien = ");
-        TenNhanVien = Console.ReadLine();
-        System.Console.Write("Age = ");
-        Age = Convert.ToInt16(Console.ReadLine());
+        MaNhanVien = "k64";
+        TenNhanVien = "Nguyen Hong Quan";
+        Address = "Quang Ninh";
+        Age = 23;
         
     }
     
-    public void Display()
+    public void Display2(string ten, string id, string diachi, int tuoi)
         {
-            System.Console.WriteLine("{0} - {1} - {2} tuoi", MaNhanVien, TenNhanVien, Age);
+            System.Console.WriteLine("Nhan Vien {0} - {1} - {2} - {3} tuoi", MaNhanVien, TenNhanVien, Address, Age);
+        }
+
+    public int GetYearOfBirth (int Age)
+        {
+            int YearOfBirth = 2023 - Age;
+            return YearOfBirth;
         }
     }
 }
--- Project/NewApp/Program.cs
+++ Project/NewApp/Program.cs
@@ -4,15 +4,15 @@ public class Program
 {
     private static void Main(string[] args)
     {
-        Person std1 = new Person();
+        Employee epl1 = new Employee();
         
         
         string ten = "Nguyen Hong Quan";
-        string id = "1921050489";
+        string id = "k64";
         string diachi = "Quang Ninh";
         int tuoi = 22;
-        Console.WriteLine("{0} sinh nam {1}", ten, id, diachi, std1.GetYearOfBirth(tuoi));
-        std1.Display2(ten, id, diachi, tuoi);
+        Console.WriteLine("{0} sinh nam {1}", ten, id, diachi, epl1.GetYearOfBirth(tuoi));
+        epl1.Display2(ten, id, diachi, tuoi);
 
         
         
