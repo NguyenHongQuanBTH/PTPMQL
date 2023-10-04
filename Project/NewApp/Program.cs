5ff2196b7133e1dbd9a5edc1d977ecc48d96bb38
NguyenHongQuanBTH <quanto190301@gmail.com>
2023-10-04 21:29:18 +0700

    Fruit

--- Project/NewApp/Program.cs
+++ Project/NewApp/Program.cs
@@ -4,17 +4,29 @@ public class Program
 {
     private static void Main(string[] args)
     {
-        Student std1 = new Student();
-        Student std2 = new Student();
-        std1.FullName = "Nguyen Hong Quan";
-        std1.StudentID = "1921050489";
-        std1.Address = "Quang Ninh";
-        std1.Age = 23;
-        std1.Display();
-        std2.FullName = "Nguyen Duy Anh";
-        std2.StudentID = "2021050489";
-        std2.Address = "Ha Noi";
-        std2.Age = 22;
-        std2.Display();
+        Fruit fr1 = new Fruit();
+        Fruit fr2 = new Fruit();
+        Fruit fr3 = new Fruit();
+
+        fr1.NameFruit = "Apple";
+        fr1.FruitID = "fr001";
+        fr1.Address = "Quang Ninh";
+        fr1.Color = "Red";
+        fr1.Flavor = "Sweet";
+        fr1.Display();
+
+        fr2.NameFruit = "Lemon";
+        fr2.FruitID = "fr002";
+        fr2.Address = "Ha Noi";
+        fr2.Color = "Green";
+        fr2.Flavor = "Sour";
+        fr2.Display();
+
+        fr3.NameFruit = "Chilli";
+        fr3.FruitID = "fr003";
+        fr3.Address = "Hai Duong";
+        fr3.Color = "Red";
+        fr3.Flavor = "Spicy";
+        fr3.Display();
     }
 }
