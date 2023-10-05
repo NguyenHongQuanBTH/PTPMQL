02ce4b00e8e05225becdcf0ac01b77782ccba63b
NguyenHongQuanBTH <quanto190301@gmail.com>
2023-10-05 13:55:50 +0700

    Employee ke thua Person

--- Project/NewApp/Models/Employee.cs
+++ Project/NewApp/Models/Employee.cs
@@ -1,32 +1,18 @@
 namespace NewApp.Models
 {
-    public class Employee
+    public class Employee : Person
     {
-        public string MaNhanVien { get; set; }
-        public string TenNhanVien { get; set; }
-        public string Address { get; set; }
-        public int Age { get; set; }
-        
-
-    
-    public Employee()
+        public string EmployeeCode { get; set; }
+        public void EnterData()
     {
-        MaNhanVien = "k64";
-        TenNhanVien = "Nguyen Hong Quan";
-        Address = "Quang Ninh";
-        Age = 23;
-        
+        base.EnterData();
+        System.Console.Write("EmployeeCode = ");
+        EmployeeCode = Console.ReadLine();
     }
-    
-    public void Display2(string ten, string id, string diachi, int tuoi)
-        {
-            System.Console.WriteLine("Nhan Vien {0} - {1} - {2} - {3} tuoi", MaNhanVien, TenNhanVien, Address, Age);
-        }
-
-    public int GetYearOfBirth (int Age)
-        {
-            int YearOfBirth = 2023 - Age;
-            return YearOfBirth;
-        }
+    public void Display()
+    {
+        base.Display();
+        System.Console.Write("Ma nhan vien: {0}", EmployeeCode);
+    }
     }
 }
--- Project/NewApp/Program.cs
+++ Project/NewApp/Program.cs
@@ -4,9 +4,9 @@ public class Program
 {
     private static void Main(string[] args)
     {
-        Student std1 = new Student();
-        std1.EnterData();
-        std1.StudentCode = "1921050489";
-        std1.Display();
+        Employee epl1 = new Employee();
+        epl1.EnterData();
+        
+        epl1.Display();
     }
 }
