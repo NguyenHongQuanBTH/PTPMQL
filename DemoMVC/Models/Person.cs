334c8244df3dd108242145bb21c97355f0102377
NguyenHongQuanBTH <quanto190301@gmail.com>
2023-11-02 13:34:18 +0700

    Person_Models_Slide20

--- DemoMVC/Models/Person.cs
+++ DemoMVC/Models/Person.cs
@@ -5,7 +5,6 @@ public class Person
 {
     public string PersonID { get; set; }
     public string FullName { get; set; }
-    [DataType(DataType.Date)]
     public string Address { get; set; }
     
 }
